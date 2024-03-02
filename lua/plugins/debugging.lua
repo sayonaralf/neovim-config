6fa9584e85dc50bf808c8fbff614b458479a6d97
wuxu <wuxu@xiaoice.com>
2024-03-02 17:29:08 +0800

    init config.

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,62 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"leoluz/nvim-dap-go",
+		"rcarriga/nvim-dap-ui",
+	},
+	config = function()
+		require("dapui").setup()
+		require("dap-go").setup()
+		local dap, dapui = require("dap"), require("dapui")
+		dap.adapters.lldb = {
+			type = "executable",
+			command = "/usr/bin/lldb", -- adjust as needed, must be absolute path
+			name = "lldb",
+		}
+		dap.configurations.rust = {
+			{
+				-- ... the previous config goes here ...,
+				initCommands = function()
+					-- Find out where to look for the pretty printer Python module
+					local rustc_sysroot = vim.fn.trim(vim.fn.system("rustc --print sysroot"))
+
+					local script_import = 'command script import "'
+						.. rustc_sysroot
+						.. '/lib/rustlib/etc/lldb_lookup.py"'
+					local commands_file = rustc_sysroot .. "/lib/rustlib/etc/lldb_commands"
+
+					local commands = {}
+					local file = io.open(commands_file, "r")
+					if file then
+						for line in file:lines() do
+							table.insert(commands, line)
+						end
+						file:close()
+					end
+					table.insert(commands, 1, script_import)
+
+					return commands
+				end,
+				-- ...,
+			},
+		}
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
+		vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
+		vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
+		vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+	end,
+}
---
+++ lua/vim-options.lua
@@ -0,0 +1,29 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.wo.number = true
